4faa960f7fc1b82748005964155cf3802bf7e87c
Data Analyst <abusufian.tex.cu@gmail.com>
2024-02-07 05:40:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.image("Real_DL_architect.gif", use_column_width=True)
 st.title("3D Natural Image Classification App")
 st.write("This app demonstrates Image classification into different classes using web application.")
 st.markdown("<span style='color:blue'>Author Md Abu Sufian</span>", unsafe_allow_html=True)
-st.write( " Visualisation of Design and Coding Under Construction")
+st.write( " ......Visualisation of Design and Coding Under Construction.........")
   
 @st.cache(allow_output_mutation=True)
 def download_model(url, model_name):
