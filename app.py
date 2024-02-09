8d70ef120b39d12d48ccc5f2cb730dd941965017
Data Analyst <abusufian.tex.cu@gmail.com>
2024-02-09 06:35:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from PIL import Image, UnidentifiedImageError
 
 # Define class names and base URL for example images
 class_names = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
-BASE_IMAGE_URL = "https://github.com/datascintist-abusufian/Web-application-by-CNN-model-on-natural-Image-data"
+BASE_IMAGE_URL = "https://raw.githubusercontent.com/datascintist-abusufian/Web-application-by-CNN-model-on-natural-Image-data/main/cifar_image_ship_1.png"
 
 # Display the app header and description
 st.image("Real_DL_architect.gif", use_column_width=True)
